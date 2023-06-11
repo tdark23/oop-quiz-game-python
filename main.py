7eb2198b0824522c05813523e9c42d31829d658d
Ted_Mbang <89210225+tdark23@users.noreply.github.com>
2023-06-11 05:53:18 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+question_bank = []
+
+for q in question_data:
+    new_q = Question(q["text"], q["answer"])
+    question_bank.append(new_q)
+
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+quiz.results()
+
